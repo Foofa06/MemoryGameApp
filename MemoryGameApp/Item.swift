a559d996dbc1687de6d2ba36cb9fdb8b40dba626
Foofa <fdjass06@gmail.com>
2026-02-26 21:44:37 -0500

    Initial Commit

---
+++ MemoryGameApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MemoryGameApp
+//
+//  Created by Fawzia Jassim on 2/26/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ MemoryGameAppTests/MemoryGameAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameAppTests.swift
+//  MemoryGameAppTests
+//
+//  Created by Fawzia Jassim on 2/26/26.
+//
+
+import Testing
+@testable import MemoryGameApp
+
+struct MemoryGameAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
